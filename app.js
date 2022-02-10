1719d0517f5eeeafb1e5b391bd7df1616858dff6
verruu <94147656+verruu@users.noreply.github.com>
2022-02-10 19:56:06 +0100

    Initial commit, .env .gitignore

---
+++ app.js
@@ -0,0 +1,82 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+// this encryption plugin must be placed before the mongoose model it refers to
+userSchema.plugin(encrypt, {
+  // this uses the .env module to hide sensitive keys
+  secret: process.env.SECRET,
+  encryptedFields: ["password"]
+});
+
+const User = new mongoose.model("User", userSchema);
+
+//TODO
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
+});
